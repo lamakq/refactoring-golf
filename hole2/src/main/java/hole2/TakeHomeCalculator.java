bceb78397355dcc594753149cb0b447a50239a77
lamak <lamak.qaizar@careem.com>
2020-04-21 15:19:19 +0500

    hole 2 (1 point) - REVERTED: removed generic types from money [RED]

--- hole2/src/main/java/hole2/TakeHomeCalculator.java
+++ hole2/src/main/java/hole2/TakeHomeCalculator.java
@@ -11,24 +11,24 @@ class TakeHomeCalculator {
         this.percent = percent;
     }
 
-    Money netAmount(Money first, Money... rest) {
+    Money<Integer, String> netAmount(Money<Integer, String> first, Money<Integer, String>... rest) {
 
-        List<Money> monies = Arrays.asList(rest);
+        List<Money<Integer, String>> monies = Arrays.asList(rest);
 
-        Money total = first;
+        Money<Integer, String> total = first;
 
-        for (Money next : monies) {
+        for (Money<Integer, String> next : monies) {
             if (!next.second.equals(total.second)) {
                 throw new Incalculable();
             }
         }
 
-        for (Money next : monies) {
+        for (Money<Integer, String> next : monies) {
             total = new Money<>(total.first + next.first, next.second);
         }
 
         Double amount = total.first * (percent / 100d);
-        Money tax = new Money<>(amount.intValue(), first.second);
+        Money<Integer, String> tax = new Money<>(amount.intValue(), first.second);
 
         if (!total.second.equals(tax.second)) {
             throw new Incalculable();
@@ -36,11 +36,11 @@ class TakeHomeCalculator {
         return new Money<>(total.first - tax.first, first.second);
     }
 
-    static class Money {
-        final Object first;
-        final Object second;
+    static class Money<Integer, String> {
+        final Integer first;
+        final String second;
 
-        Money(Object first, Object second) {
+        Money(Integer first, String second) {
             this.first = first;
             this.second = second;
         }
