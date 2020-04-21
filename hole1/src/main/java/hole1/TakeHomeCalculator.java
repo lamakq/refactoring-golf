19f1c8eaab5be0106d3e820630f040b959b4cfbf
lamak <lamak.qaizar@careem.com>
2020-04-21 13:48:16 +0500

    hole 1 (1 point) - replaced == with equals

--- hole1/src/main/java/hole1/TakeHomeCalculator.java
+++ hole1/src/main/java/hole1/TakeHomeCalculator.java
@@ -30,7 +30,7 @@ class TakeHomeCalculator {
         Double amount = total.first * (percent / 100d);
         Pair<Integer, String> tax = new Pair<>(amount.intValue(), first.second);
 
-        if (total.second == tax.second) {
+        if (total.second.equals(tax.second)) {
             return new Pair<>(total.first - tax.first, first.second);
         } else {
             throw new Incalculable();
